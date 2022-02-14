86cf069d40df2fcd05604bd6777fbe47cf662fd6
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2022-02-14 18:26:37 +0100

    first commit

---
+++ database/seeds/VideogameSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class VideogameSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// GOAL: come detto a lezione, lo scopo del gioco e' 
+// generare una semplice pagina home rappresentante l'entita' Videogame (titolo, sottotilo, rating) dotata di login. 
+// La rappresentazione dell'enetita' viene fatta in Vue. Gli utenti collegati hanno la possibilita' di eliminare un'entita'.
+// In fase di eliminazione viene mandata una mail all'utante registrato e una all'amministratore (inventare una mail di default) riportando l'entita' eliminata.
+
+
+
+
+
+
+Route::get('/home', 'HomeController@home') ->name('home');
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+Route::post('/login', 'Auth\LoginController@login') ->name('login');
+Route::post('/register', 'Auth\RegisterController@register') ->name('register');
+Route::get('/logout', 'Auth\LoginController@logout') ->name('logout');
+
