3a18a33d41de368d7f4b55e0b02af687b50f537d
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2022-02-15 08:05:16 +0100

    login register logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,38 @@
 <header>
     <h1>Videogames</h1>
+
+    @auth
+        <h1>Hello {{Auth::user()->name}}</h1>
+            <a href="{{route('logout')}}" class="btn btn-danger">Logout</a>
+    @else
+        <h2>registrati</h2>
+        <form action="{{route('login')}}" method="post">
+        @method('post')
+        @csrf
+
+        <label for="email">Email</label>
+        <input type="email" name="email">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+        <button type="submit" class="btn btn-success">Login</button>
+        </form>
+        
+        
+        <h2>Registrati</h2>
+        <form action="{{route('register')}}" method="post">
+            @method('post')
+            @csrf
+
+            <label for="name">Name</label>
+            <input type="text" name="name">
+            <label for="email">Email</label>
+            <input type="email" name="email">
+            <label for="password">Password</label>
+            <input type="password" name="password">
+            <label for="password_confirmation">Password Confirmation</label>
+            <input type="password" name="password_confirmation">
+            <button type="submit" class="btn btn-success">Register</button>
+        </form>
+
+    @endauth
 </header>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.main-layout')
 @section('content')
-<h1>hello</h1>
 <Videogames-component></videogames-component>
 
 @endsection
